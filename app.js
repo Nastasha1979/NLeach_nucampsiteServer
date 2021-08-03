155ff2a79b1eca5986d7dfb3b25828d06176e43c
Nastasha Leach <nastasha.leach@outlook.com>
2021-08-03 17:10:11 -0500

    Added user registration, login, and logout functions

--- app.js
+++ app.js
@@ -42,33 +42,22 @@ app.use(session({
   resave: false,
   store: new FileStore()
 }));
+
+//so users can access these pages before authentication these have been moved up
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
 //authentication
 function auth(req, res, next){ 
     console.log(req.sessions);
 
     if(!req.session.user){
-      const authHeader = req.headers.authorization;
-      if(!authHeader) {
-        const err = new Error("You are not authenticated");
-        res.setHeader("WWW-Authenticate", "Basic");
-        err.status = 401;
-        return next(err);
-      }
+      const err = new Error("You are not authenticated");
+      err.status = 401;
+      return next(err);
 
-      const auth = Buffer.from(authHeader.split(" ")[1], "base64").toString().split(":");
-      const user = auth[0];
-      const pass = auth[1];
-      if(user === "admin" && pass === "password") {
-        req.session.user = "admin";
-        return next();  //authorized
-      } else {
-        const err = new Error("You are not authenticated.");
-        res.setHeader("WWW-Authenticate", "Basic");
-        err.status = 401;
-        return next(err);
-      }
     } else {
-      if(req.session.user === "admin") {
+      if(req.session.user === "authenticated") {
         return next();
       } else {
         const err = new Error("You are not authenticated.");
@@ -81,8 +70,7 @@ function auth(req, res, next){
 app.use(auth);
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionsRouter);
 app.use('/partners', partnersRouter);
