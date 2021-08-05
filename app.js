066be93c50c6c98be9f798ce071644482efb2b8d
Nastasha Leach <nastasha.leach@outlook.com>
2021-08-04 21:14:47 -0500

    Added passport, passport-local, and passport-local-mongoose. Incorporated

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const session = require("express-session");
 const FileStore = require("session-file-store")(session);
+const passport = require("passport");
+const authenticate = require("./authenticate");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -43,28 +45,24 @@ app.use(session({
   store: new FileStore()
 }));
 
+//to initialize sessions for users. only necessary if you are using sessions
+app.use(passport.initialize());
+app.use(passport.session());
 //so users can access these pages before authentication these have been moved up
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 //authentication
-function auth(req, res, next){ 
-    console.log(req.sessions);
+function auth(req, res, next) {
+  console.log(req.user);
 
-    if(!req.session.user){
-      const err = new Error("You are not authenticated");
+  if (!req.user) {
+      const err = new Error('You are not authenticated!');                    
       err.status = 401;
       return next(err);
-
-    } else {
-      if(req.session.user === "authenticated") {
-        return next();
-      } else {
-        const err = new Error("You are not authenticated.");
-        err.status = 401;
-        return next(err);
-      }
-    }
+  } else {
+      return next();
+  }
 }
 
 app.use(auth);
