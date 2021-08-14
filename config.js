287649e9bf8b419a1c61e4355b394de513435304
Nastasha1979 <31832166+Nastasha1979@users.noreply.github.com>
2021-08-14 12:25:21 -0500

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    "secretKey": "12345-67890-09876-54321",
-    "mongoUrl" : "mongodb://localhost:27017/nucampsite",
-    "facebook" : {
-        clientId: "4139125729458196",
-        clientSecret : "593e3a414afa26a9797d722c60f18610"
-    }
-}
