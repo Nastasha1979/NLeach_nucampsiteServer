8241ad68b04f4e5490cfd3395b15b7d86a471975
Nastasha Leach <nastasha.leach@outlook.com>
2021-08-14 11:51:44 -0500

    index commit

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     "secretKey": "12345-67890-09876-54321",
     "mongoUrl" : "mongodb://localhost:27017/nucampsite",
     "facebook" : {
-        clientId: "858459111754219",
-        clientSecret : "0e19f2bebd7a934327f87d6ea7f4e23c"
+        clientId: "4139125729458196",
+        clientSecret : "593e3a414afa26a9797d722c60f18610"
     }
 }
